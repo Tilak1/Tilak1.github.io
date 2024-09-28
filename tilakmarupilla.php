836ba21820a7b939cb1954519f82147cc30609cd
Tilak Marupilla <tilakmarupilla67@gmail.com>
2024-09-28 14:34:43 -0400

    Create tilakmarupilla.php

---
+++ tilakmarupilla.php
@@ -0,0 +1,310 @@
+<!DOCTYPE html PUBLIC "-//W3C//DTD HTML 4.01//EN" "http://www.w3.org/TR/html4/strict.dtd">
+
+<html>
+<head>
+
+    <meta content="text/html; charset=utf-8" http-equiv="Content-Type">
+    <!-- #BeginEditable "doctitle" -->
+
+    <title>Ali Saeizadeh | WINES</title>
+    <!-- #EndEditable -->
+
+    <meta content="..." name="Description">
+
+    <meta content="..." name="Keywords">
+
+    <meta content="all,follow" name="robots">
+
+    <meta content="..." name="author">
+    <!-- Javascript -->
+
+    <meta content="text/javascript" http-equiv="Content-Script-Type">
+
+    <!-- CSS -->
+
+    <link href="css/style.css" media="screen, projection, tv" rel="stylesheet" type="text/css">
+    <link href="css/style-print.css" media="print" rel="stylesheet" type="text/css">
+    <!-- Favicon -->
+
+    <link href="favicon.ico" rel="shortcut icon" type="image/x-icon">
+
+    <style type="text/css">
+
+        .style3 {
+
+            text-align: left;
+
+        }
+
+        .style4 {
+
+            list-style-type: square;
+
+        }
+
+    </style>
+</head>
+<body>
+
+
+<div id="wrapper">
+
+    <!-- Header -->
+
+    <!-- Header -->
+    <div id="bar">
+        <img alt="Northeastern University" src="images/logo_neu.png">
+        <a> Wireless Networks and Embedded Systems Lab</a>
+    </div>
+    <div id="header">
+
+        <!-- Your website name  -->
+        <a href="index.php"><img alt="Wireless Networks and Embedded Systems Laboratory" src="images/logo.png"
+                                 width="980"></a>
+
+        <!-- Your website name end --></div>
+    <!-- Header end -->
+    <!-- Header end -->
+
+    <!-- Menu -->
+
+    <!-- Menu --><a class="hidden" href="#skip-menu">Skip menu</a>
+    <ul id="menu" class="cleaning-box">
+        <li class="first">
+            <a href="index.php">Home</a></li>
+        <li><a>|</a></li>
+        <li><a class='active' href="people.php">People</a></li>
+        <li><a>|</a></li>
+        <li><a href="research.php">Research</a></li>
+        <li><a>|</a></li>
+        <li><a href="publications.php">Publications</a></li>
+        <li><a>|</a></li>
+        <li><a href="outreach.php">Outreach</a></li>
+        <li><a>|</a></li>
+        <li><a href="innovation.php">Innovation</a></li>
+        <!--
+        <li><a>|</a> </li>
+        <li><a  href="gallery.php">Gallery</a></li>
+        <li><a>|</a> </li>
+        <li><a  href="openbeam.php">OpenBeam</a></li>	-->
+    </ul>
+    <!-- Menu end -->
+    <!-- Menu end -->
+
+    <hr class="noscreen">
+
+    <div id="content-box">
+
+        <div id="content-box-in">
+
+            <a name="skip-menu"></a>
+
+            <div class="content-box-left-in">
+
+                <a name="skip-menu"></a>
+
+                <div class="gallery">
+
+                    <a name="skip-menu"></a><a href="#">
+
+                        <img alt="Ali Saeizadeh" class="people" height="225" src="people/Ali/ali.jpg"></a>
+
+                    <h3 class="style3"><strong>Ali Saeizadeh</strong></h3>
+
+                    <p id="small_title"><strong>Ph.D. Student in Electrical Engineering</strong></p>
+
+                    <p id="small_title"><a href="https://www.northeastern.edu/wiot">Institute for the Wireless Internet
+                            of Things</a></p>
+
+                    <p id="address">Department of Electrical and Computer Engineering<br>
+                        Northeastern University
+                    </p>
+
+                    <p>
+                        <em><strong>Office</strong>: </em>
+                        640 Northeastern University EXP, 805 Columbus Ave, Boston, MA 02120
+                        <a target="_blank" href="https://maps.app.goo.gl/fGhHpAnVcUCZG24H9"
+                           style="border:0;box-shadow: 0 0 0 0">
+                            <img src="images/pin.png"
+                                 style="margin-left:5px; margin-top:0; border:none; box-shadow: 0 0 0 0; text-decoration:none; position:absolute;"
+                                 width="10px"></img>
+                        </a>
+                        <br>
+                        <em><strong>Mail</strong>: </em>
+                        640 Northeastern University EXP, 805 Columbus Ave, Boston, MA 02120<br>
+                    </p>
+
+                    <em><strong>Email</strong>: </em>
+                    <a href="mailto:saeizadeh.a@northeastern.edu"><em>saeizadeh.a@northeastern.edu</em></a>
+
+                </div>
+
+                <br>
+
+                <!-- <br>
+
+                <br> -->
+
+                <a style="text-decoration:none;" href="https://www.northeastern.edu/wiot" target="_blank"><img
+                            alt="NU IoT" class="Linkedin.png" src="images/whiteslideslogotransparent.png" height="60">
+                </a>
+
+                &nbsp;&nbsp;
+
+                <a style="text-decoration:none;" href="https://scholar.google.com/citations?user=slunWekAAAAJ&hl=en"
+                   target="_blank"><img alt="Google Scholar Profile" class="LinkedIn_emrecan"
+                                        src="images/google_scholar_3.png" height="50"> </a>
+
+                &nbsp;&nbsp;
+
+                <a style="text-decoration:none;" href="https://www.linkedin.com/in/ali-saeizadeh/" target="_blank"><img
+                            alt="LinkedIn Profile" class="LinkedIn_emrecan" src="images/Linkedin_new.png" height="50">
+                </a>
+                <br>
+
+                <br>
+
+                <h3><a name="biography">Biography</a></h3>
+
+                <p class="text-content">
+                  Ali Saeizadeh is a Ph.D. student at 
+                  <a href="https://www.northeastern.edu/wiot">Institute for the Wireless Internet of Things</a> 
+                  at Northeastern University, USA, under Prof. <a href="https://ece.northeastern.edu/wineslab/tmelodia.php">Tommaso Melodia</a>.
+                  In 2022, he obtained his Bachelor's degree in Electrical Engineering at the 
+                  <a href="https://ece.ut.ac.ir/en">University of Tehran</a>.
+                  His main research efforts are focused on the application of AI and machine learning in wireless communication, digital twin technology, embedded deep learning applications, and next-generation wireless communication systems.
+                </p>
+
+
+
+                <h3><a name="research">Research Interests</a></h3>
+
+                <div class="content-box-left-in">
+
+                    <ul class="style4">
+
+                        <li>
+
+                            <p>Deep Learning for Wireless Communications</p>
+
+                        </li>
+
+                        <li>
+
+                            <p>Internet of Things</p>
+
+                        </li>
+
+                        <li>
+
+                            <p>AI/ML</p>
+
+                        </li>
+
+                    </ul>
+
+                </div>
+<!--                <h3><a name="thesis">Master Thesis</a></h3>
+
+                <p class="text-content">
+                    Bluetooth Low Energy mesh networks are emerging as the new standard of
+                    short burst communications.
+                    While security of the messages is guaranteed thought some standard
+                    encryption of the messages, a little has been done in terms of the
+                    protecting the overall network security status in case of attacks.
+                </p>
+                <p class="text-content">
+                    Indeed, a lot of the classical methods of network analysis require an huge
+                    amount of data describing both a legitimate and an attack situations and at
+                    the current state of the art there are no public available datasets
+                    containing attacks.
+                    In order to create a reliable mechanism of network analysis suited for BLE
+                    in this paper we propose a machine learning Intrusion Detection System
+                    (IDS) based on pattern classification and recognition of the most classical
+                    denial of service attacks affecting this kind of networks.
+                </p>
+                <p class="text-content">
+                    Moreover, in order to overcome the gap created by the absence of data, we
+                    present our data collection system based on ESP32 that allowed the
+                    collection of the packets from the Network and the Model layers of the BLE
+                    Mesh stack and the experiments conducted to get the necessary data to train
+                    the IDS.
+                </p>
+                <p class="text-content">
+                    After the description of the IDS and the data collector, we describe the
+                    results obtained by the experimental setups and we discuss the results of
+                    the Intrusion Detection System, commenting on its strengths and the sides
+                    where more analysis is needed, proposing for future work new approaches
+                    related to the same classification model proposed.
+                </p>
+-->
+                <h3><a name="publications">Publications</a></h3>
+
+                <h4>Articles in Conference Proceedings</h4>
+
+                <div class="content-box-left-in">
+
+                    <ul class="style4">
+
+                    <li style="text-align: justify;">
+                    <p>A. Saeizadeh, D. Schonholtz, D. Uvaydov, R. Guida, E. Demirors, P. Johari, J. M. Jimenez, J. S. Neimat, T. Melodia,
+                            <em>"SeizNet: An AI-Enabled Implantable Sensor Network System for Seizure Prediction,"</em>
+                            <strong>Proc. of  IEEE/IFIP Wireless On-demand Network systems and Services Conference (WONS)</strong>, Chamonix, France, January 2024.
+                            <strong>
+                            <a href="https://arxiv.org/pdf/2401.06644" target="_blank">[pdf]</a>
+                            <a href="wines_bibtex/saeizadeh2023wons.txt" target="_blank">[bibtex]</a>
+                            </strong>
+                            </p>
+                        </li>
+                    </ul>
+
+                </div>
+
+                <h3><a name="links">Links</a></h3>
+
+                <div class="content-box-left-in">
+
+                    <ul class="style4">
+
+                        <li>
+                            <p>
+                                <a href="https://scholar.google.com/citations?user=slunWekAAAAJ&hl=en">Google Scholar</a>
+                            </p>
+                        </li>
+
+                        <li>
+                            <p>
+                                <a href="https://www.linkedin.com/in/ali-saeizadeh/">Linkedin</a>
+                            </p>
+                        </li>
+
+                        <li>
+                            <p>
+                                <a href="https://github.com/Alol2565">GitHub</a>
+                            </p>
+                        </li>
+
+                    </ul>
+
+                </div>
+
+                <br>
+
+            </div>
+        </div>
+    </div>
+
+    <div class="cleaner">&nbsp;</div>
+    <hr class="noscreen">
+
+    <!-- Footer -->
+    <?php
+     include("footer.php");
+     ?>
+    <!-- Footer end -->
+</div>
+</div>
+
+</body>
+
+</html>
